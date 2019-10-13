25ea49e244e1d85b73542271067690878af3dcf8
Oleg Peresada <peresada.oleg@mail.ru>
2019-10-13 22:33:12 +0600

    juj

---
+++ Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace deploy
+{
+  [TestFixture]
+  public class Class
+  {
+    public void TestMethodFoo_False()
+    {
+      Assert.False(true);
+    }
+  }
+}
